16f8d510da629121d3a352a4bceb5762874b2b09
Piyush Ghai <ghai.8@osu.edu>
2018-07-24 11:23:15 -0700

    Added test cases for recv_msg function

--- mms/model_service_worker.py
+++ mms/model_service_worker.py
@@ -140,7 +140,9 @@ class MXNetModelServiceWorker(object):
         except (IOError, OSError) as sock_err:
             raise MMSError(err.RECEIVE_ERROR, "{}".format(sock_err.message))
         except ValueError as v:
-            raise MMSError(err.INVALID_REQUEST, "JSON message format error: {}".format(v))
+            raise MMSError(err.INVALID_MESSAGE, "JSON message format error: {}".format(v))
+        except MMSError as error:  # The error raise in #135 goes into the Exception block below and we lose the
+            raise error           # status code
         except Exception as e:  # pylint: disable=broad-except
             raise MMSError(err.UNKNOWN_EXCEPTION, "{}".format(e))
 
